e19097e0befb62ea5573454df60c222d6b40eb4a
vinh15cm <147390597+vinh15cm@users.noreply.github.com>
2024-05-13 07:41:19 +0700

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,20 @@
+import Bai1 from "./components/Bai1"
+import Bai2 from "./components/Bai2"
+import Bai3 from "./components/Bai3"
+import Bai4 from "./components/Bai4"
+import Bai5 from "./components/Bai5"
+import Bai6 from "./components/Bai6"
+
+export default function App() {
+  return (
+    <div>
+      <Bai1></Bai1>
+      <Bai2></Bai2>
+      <Bai3></Bai3>
+      <Bai4></Bai4>
+      <Bai5></Bai5>
+      <Bai6></Bai6>
+
+    </div>
+  )
+}
